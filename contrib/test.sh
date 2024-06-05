9c5e6e27a7b6c33fffa0583708e7ad01e2646c69
Kohei Taniguchi <kohei@chaintope.com>
2024-06-05 11:25:42 +0900

    Bump up the MSRV to 1.56.1
    port from https://github.com/rust-bitcoin/rust-bitcoin/commit/d9cc724187def13232602955650e922224880d97#diff-08c82c1ad26f9227b39053148c4d1dc8c50b476861ef07bf54f7e460a394b98d

--- contrib/test.sh
+++ contrib/test.sh
@@ -4,27 +4,6 @@ set -ex
 
 CRATES="tapyrus hashes internals fuzz"
 DEPS="recent minimal"
-MSRV="1\.48\.0"
-
-# Test pinned versions.
-if cargo --version | grep ${MSRV}; then
-    cargo update -p serde_json --precise 1.0.99
-    cargo update -p serde --precise 1.0.156
-    cargo update -p quote --precise 1.0.30
-    cargo update -p proc-macro2 --precise 1.0.63
-    cargo update -p serde_test --precise 1.0.175
-    # Have to pin this so we can pin `schemars_derive`
-    cargo update -p schemars --precise 0.8.12
-    # schemars_derive 0.8.13 uses edition 2021
-    cargo update -p schemars_derive --precise 0.8.12
-    # memchr 2.6.0 uses edition 2021
-    cargo update -p memchr --precise 2.5.0
-    # byteorder 1.5.0 uses edition 2021
-    cargo update -p byteorder --precise 1.4.3
-
-    # Build MSRV with pinned versions.
-    cargo check --all-features --all-targets
-fi
 
 for dep in $DEPS
 do
